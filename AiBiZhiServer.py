837ecec08105aec3bade11a94faaa80294d32b53
chiangkai <chiangkk@163.com>
2022-09-04 19:04:57 +0800

    回到单线程版本，多线程无明显提升

--- AiBiZhiServer.py
+++ AiBiZhiServer.py
@@ -1,10 +1,9 @@
 import random
-from multiprocessing import JoinableQueue as Queue
-from multiprocessing import Process
+
 import requests
 import json
 import os
-from threading import Lock, Thread
+import time
 
 
 class AiBiZhiServer:
@@ -36,17 +35,14 @@ class AiBiZhiServer:
         self.totalCount = None
         self.cateName = None
         self.cateId = None
-        self.downloadCount = 1
+        self.downloadCount = None
         self.totalDownloadCount = None
-        self.countLock = Lock()
-        self.downloadImgUrls = Queue()
-        self.cateNum = 0
-        self.cateNumLock = Lock()
 
-    def run(self, cateNum, totalDownload, threadCount=8):
+    def run(self, cateNum, totalDownload, userSleep=False):
         """
         :param cateNum 分类参数
         :param totalDownload 总下载数
+        :param userSleep 是否下载时睡眠
         :rtype void 无返回
         """
         saveImgPath = os.getenv('saveImgPath')
@@ -57,24 +53,28 @@ class AiBiZhiServer:
         self.totalCount = self.cateInfo['count']
         self.cateName = self.cateInfo['name']
         self.cateId = self.cateInfo['id']
-
+        self.downloadCount = 0
         self.totalDownloadCount = totalDownload
         savePath = '{}/{}'.format(self.saveImgPath, self.cateName)
         if os.path.exists(savePath) is False:
             os.makedirs(savePath)
-        task = []
-        # 2个线程获取imageUrls
-        for i in range(5):
-            task.append(Process(target=self.pushQueueImageUrl))
+        now = 0
+        while now * 20 < totalDownload:
+            cateUrl = self.getDeclareCategoryUrl(now * 20)
+            imageUrls = self.getImageUrl(cateUrl)
+            if len(imageUrls) == 0:
+                print("未加载到图片资源,程序结束")
+                exit(1)
+            needSleep = False
+            for imageUrl in imageUrls:
+                self.downloadCount += 1
+                needSleep = self.downloadImage(imageUrl)
+            if needSleep and userSleep:
+                sleepTime = random.randint(5, 10)
+                print("休息{}s".format(sleepTime))
+                time.sleep(sleepTime)
 
-        for i in range(threadCount):
-            task.append(Process(target=self.customImageUrl))
-        for i in task:
-            i.daemon = True
-            i.run()
-            # i.join()
-
-        # self.downloadImgUrls.join()
+            now += 1
 
     def getCategoreyUrl(self):
         return self.baseUrl + self.categoryPath
@@ -90,7 +90,8 @@ class AiBiZhiServer:
     def getDeclareCategoryUrl(self, skip):
         totalCount = self.totalCount  # 总图数
         if skip > totalCount:
-            return None
+            print('已近到达下载上线或已超过图库上线')
+            exit(0)
         skipInfo = ''
         if skip > 0:
             skipInfo = '?skip={}'.format(skip)
@@ -99,43 +100,20 @@ class AiBiZhiServer:
         return cateUrl
 
     def getImageUrl(self, cateUrl):
-        if cateUrl is None:
-            return []
         resp = requests.get(cateUrl, headers=self.headers)
         respJson = json.loads(resp.content)
         imageUrls = respJson['res']['wallpaper']
         print("get ImageUrls {}".format(imageUrls))
         return imageUrls
 
-    def pushQueueImageUrl(self):
-        while True:
-            with self.cateNumLock:
-                cateNum = self.cateNum
-                self.cateNum = self.cateNum + 20
-            if (cateNum >= self.totalDownloadCount):
-                self.downloadImgUrls.put(None)
-                break
-            cateUrls = self.getDeclareCategoryUrl(cateNum)
-            imageUrls = self.getImageUrl(cateUrls)
-            if (len(imageUrls) == 0):
-                self.downloadImgUrls.put(None)
-                break
-            for i in imageUrls:
-                self.downloadImgUrls.put(i)
-        print("push over")
-
     def downloadImage(self, imageUrl):
         if self.downloadCount > self.totalDownloadCount:
             return;
-        # t = threading.current_thread()
-        t = os.getpid()
-        print("Thread id : {},正在处理{}张,图片地址:{}".format(t, self.downloadCount, imageUrl['img']))
+        print("正在处理{}张,图片地址:{}".format(self.downloadCount, imageUrl['img']))
         if os.path.exists(self.saveImgPath + '/{}/{}.jpg'.format(self.cateName, imageUrl['id'])):
             print("第{}张 {}.jpg已存在".format(self.downloadCount, imageUrl['id']))
-            with self.countLock:
-                self.downloadCount += 1;
             return False
-        print("Thread id : {},开始下载{}张图片".format(t, self.downloadCount))
+        print("开始下载{}张图片".format(self.downloadCount))
         while True:
             try:
                 resp = requests.get(imageUrl['img'], headers=self.downLoadImageHeader)
@@ -146,16 +124,4 @@ class AiBiZhiServer:
                 print(e.with_traceback())
         with open(self.saveImgPath + '/' + self.cateName + "/" + imageUrl['id'] + '.jpg', 'wb') as f:
             f.write(resp.content)
-        with self.countLock:
-            self.downloadCount += 1;
         return True
-
-    def customImageUrl(self):
-        while True:
-            url = self.downloadImgUrls.get()
-            self.downloadImgUrls.task_done()
-            if url is None:
-                print("over download ")
-                self.downloadImgUrls.put(None)
-                break;
-            self.downloadImage(url)
